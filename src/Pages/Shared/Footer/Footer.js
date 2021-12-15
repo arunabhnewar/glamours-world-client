3a9eadcfad6ac3a2aa9329a2b5ba4ddd53e2c71b
arunabhnewar <newardevil@gmail.com>
2021-12-15 16:42:17 +0600

    footer done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,13 +1,137 @@
+import { Facebook, Instagram, MailOutline, Phone, Room, Twitter, WhatsApp, YouTube } from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
-    
+    display: flex;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+const Logo = styled.h2`
+    color: teal;
+`;
+
+const Details = styled.p`
+    margin: 20px 0px;
+    color: gray;
+`;
+
+const SocialContainer = styled.div`
+    display: flex;
+`;
+
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    color: white;
+    background-color: #${(props) => props.color};
+     display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+    border-radius: 50%;
+    cursor: pointer;
+`;
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`;
+
+const Title = styled.h3`
+    color: teal;
+    margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+    width: 50%;
+    cursor: pointer;
+    color: gray;
+    margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
 `;
 
 const Footer = () => {
     return (
         <Container>
+            <Left>
+                <Logo>Glamours World</Logo>
+                <Details>The world wide web is evolving at a relentless pace, and with an accelerated growth each passing year, there is bound to be an overwhelming surge of online content. It was for this very reason that personalisation of search feeds was proposed as a solution to combat the overload of irrelevant information.</Details>
+                <SocialContainer>
+                    <SocialIcon color="55ACEE">
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color="3B5999">
+                        <Facebook />
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon color="FF0000" >
+                        <YouTube />
+                    </SocialIcon>
+                    <SocialIcon color="25D366" >
+                        <WhatsApp />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
 
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem> Home</ListItem>
+                    <ListItem> Cart</ListItem>
+                    <ListItem> Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Privacy</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <Room style={{ marginRight: "10px" }} /> 622 Dixie Path , South Tobinchester 98336
+                </ContactItem>
+                <ContactItem>
+                    <Phone style={{ marginRight: "10px" }} /> +1 234 56 78
+                </ContactItem>
+                <ContactItem>
+                    <MailOutline style={{ marginRight: "10px" }} /> contact@lama.dev
+                </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
         </Container>
     );
 };
